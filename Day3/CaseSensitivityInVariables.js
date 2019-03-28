c7aee48f362b4d84d442697aff6898a51bf2a90e
shesings <sheetalsingh7007@gmail.com>
2019-03-27 22:13:48 -0400

    Day 3 - case sensitivity

---
+++ Day3/CaseSensitivityInVariables.js
@@ -0,0 +1,15 @@
+// Declarations
+
+// using camelCasing for functions and variable naming
+
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// recommended to keep the camelCasing going
+// even now camelCase, CAMELCASE, and CamelCase will all be recognized as different
+
+// Assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
