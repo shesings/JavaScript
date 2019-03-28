e4ce9336c14a8d3055f7b9b5bf7b6612e6ffc8ba
shesings <sheetalsingh7007@gmail.com>
2019-03-27 22:08:36 -0400

    Day 3 - uninitialized variables

---
+++ Day3/UninitializedVariables.js
@@ -0,0 +1,10 @@
+// Initialize these three variables
+var a = 5; // leaving var a; --> would be undefined
+var b = 10; // if i try to perform any math or string concatenation
+var c = "I am a"; // i would get "not a number" or "undefined"
+
+// Do not change code below this line
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
