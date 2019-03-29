f5ea38f3534e0eae7b80db734c2e7a4f2857594b
shesings <sheetalsingh7007@gmail.com>
2019-03-28 22:34:33 -0400

    Day 4 - Increment/Decrement

---
+++ Day4/Decrement.js
@@ -0,0 +1,6 @@
+var myVar = 11;
+
+// Only change code below this line
+myVar = myVar - 1; // this is too long to decrement
+// can do this instead
+myVar--;
---
+++ Day4/IncrementANumber.js
@@ -0,0 +1,7 @@
+var myVar = 87;
+
+// Only change code below this line
+myVar++;
+// instead of myVar = myVar + 1;
+// tooo long
+// can just increment this waaay
