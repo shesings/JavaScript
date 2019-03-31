0e47dbd73a7d77a7673a42523c84ca6b20375cdc
shesings <sheetalsingh7007@gmail.com>
2019-03-30 22:14:05 -0400

    Day 5 - Decimals and Math

---
+++ Day5/DivideOneDecimalbyAnother.js
@@ -0,0 +1,4 @@
+var quotient = 4.4 / 2.0; 
+
+// decimal divded by decimal = decimal value
+// in this case 2.2
---
+++ Day5/FindingRemainder.js
@@ -0,0 +1,7 @@
+// Only change code below this line
+
+var remainder;
+remainder = 11 % 3;
+
+// remainder != to 'modulus'
+// similar but doesn't handle negative numbers
