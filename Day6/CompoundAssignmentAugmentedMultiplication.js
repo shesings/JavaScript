27b61fb3b7a856e4cb0038150709d0701a13c726
shesings <sheetalsingh7007@gmail.com>
2019-03-31 23:04:30 -0400

    Day 6 - Compound Operators

---
+++ Day6/CompoundAssignmentAugmentedMultiplication.js
@@ -0,0 +1,12 @@
+var a = 5;
+var b = 12;
+var c = 4.6;
+
+// Only modify code below this line
+
+a *= 5;
+b *= 3;
+c *= 10;
+// so does this take whatever variable it is,
+// and then multiply the number against it? i think so.
+
