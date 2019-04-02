f6d727480017a7f16eb33eefc42bcd68aa2c0b1e
shesings <sheetalsingh7007@gmail.com>
2019-04-01 22:17:28 -0400

    Day 7 - Strings

---
+++ Day7/EscapingLiteralQuotesInStrings.js
@@ -0,0 +1,14 @@
+// var myStr = ""; // Change this line
+
+// in order to escape literal quotes
+// used a blackslash
+// \" and \'
+
+// var sampleString = "Alan said to Dorothy, \"Get your work done now.\"";
+
+// this signals to JavaScript that the following quote is not the end of 
+// the string but instead it actually needs to appear
+
+// var myStr = "I am a \"double quoted\" string inside \"double quotes\".";
+
+var myStr = "I am a \"double quoted\" string inside \"double quotes\".";
