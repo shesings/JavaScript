22d9e106cb04d58eb1b21f13264331d97022d406
shesings <sheetalsingh7007@gmail.com>
2019-04-01 22:17:09 -0400

    Day 7 - Strings

---
+++ Day7/DeclareStringVariables.js
@@ -0,0 +1,11 @@
+// Example
+var firstName = "Alan";
+var lastName = "Turing";
+
+"Alan Turing" // called string literal
+// because its series of zero or more characters enclosed in single or
+// double quotes
+
+// Only change code below this line
+var myFirstName = "Sheetal";
+var myLastName = "Singh";
