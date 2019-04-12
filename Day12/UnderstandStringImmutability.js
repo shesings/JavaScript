149b30959d118b8fc9efc127ee361953e70878b1
shesings <sheetalsingh7007@gmail.com>
2019-04-12 09:02:28 -0400

    Day 12

---
+++ Day12/UnderstandStringImmutability.js
@@ -0,0 +1,11 @@
+// Setup
+var myStr = "Jello World";
+
+// Only change code below this line
+
+myStr = "Hello World"; // Fix Me
+
+// cannot change string literal
+// meaning cannot change by myStr[0]
+// by the actual character
+// only by assignment
