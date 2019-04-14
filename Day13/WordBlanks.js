c49ca2f8fb88eadd84617ab9b2826ba450d97d00
shesings <sheetalsingh7007@gmail.com>
2019-04-13 21:54:41 -0400

    Day 13

---
+++ Day13/WordBlanks.js
@@ -0,0 +1,19 @@
+function wordBlanks(myNoun, myAdjective, myVerb, myAdverb) {
+    // Your code below this line
+    var result = "";
+
+// var myNoun = "dog";
+// var myAdjective = "big";
+// var myVerb = "ran";
+// var myAdverb = "quickly";
+
+    result =  myAdjective + " " + myNoun + " " + myVerb + " " + myAdverb;
+
+
+    // Your code above this line
+    return result;
+}
+
+// Change the words here to test your function
+wordBlanks("dog", "big", "ran", "quickly");
+
