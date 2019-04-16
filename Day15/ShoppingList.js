578b752f5a99a76894d0fb4f9f69ef4055969988
shesings <sheetalsingh7007@gmail.com>
2019-04-15 22:21:28 -0400

    Day 15

---
+++ Day15/ShoppingList.js
@@ -0,0 +1,8 @@
+var myList = [];
+myList.push(["Choco", 1]);
+myList.push(["Choco", 1]);
+myList.push(["Choco", 1]);
+myList.push(["Choco", 1]);
+myList.push(["Choco", 1]);
+// creates 5 "sub-arrays" where the first element is a string
+// and the second element is a number
