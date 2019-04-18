058f771cfbbd781e6692de17f74aea912e45ce1e
shesings <sheetalsingh7007@gmail.com>
2019-04-18 12:51:32 -0400

    Day 18

---
+++ Day18/PracticeComparingDifferentValues.js
@@ -0,0 +1,10 @@
+// Setup
+function compareEquality(a, b) {
+    if (a === b) { // uses strictly equal now
+        return "Equal";
+    }
+    return "Not Equal";
+}
+
+// Change this value to test
+compareEquality(10, "10");
