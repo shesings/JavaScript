0c38204690bd5b71a0cbd29a702de907c75179b8
shesings <sheetalsingh7007@gmail.com>
2019-04-18 12:49:09 -0400

    Day 18

---
+++ Day18/ComparisonWithEqualityOperator.js
@@ -0,0 +1,10 @@
+// Setup
+function testEqual(val) {
+    if (val=='12') { // or can be val==12
+        return "Equal";
+    }
+    return "Not Equal";
+}
+
+// Change this value to test
+testEqual(10);
