071a84771b6ab51e520892ae13c90d3ed3e0d285
shesings <sheetalsingh7007@gmail.com>
2019-04-18 12:52:17 -0400

    Day 18

---
+++ Day18/ComparisonWithInequalityOperator.js
@@ -0,0 +1,10 @@
+// Setup
+function testNotEqual(val) {
+    if (val!=99) { // inequality operatorrr
+        return "Not Equal";
+    }
+    return "Equal";
+}
+
+// Change this value to test
+testNotEqual(10);
